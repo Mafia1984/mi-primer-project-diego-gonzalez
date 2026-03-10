5dccd3437af450bb63b598bb1f3c3852ef506f84
Mafia1984 <dgonzalezc@estud.tesa.edu.ec>
2026-03-10 15:32:52 -0500

    actualizado

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'mi.primer.ios.project',
+  appName: 'Riksiri',
+  webDir: 'dist'
+};
+
+export default config;
