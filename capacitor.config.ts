cd1f1b7d810256146180adefabf96a41ef3389be
Mafia1984 <dgonzalezc@estud.tesa.edu.ec>
2026-03-16 12:55:58 -0500

    ACTUALIZADO ULTIMO

---
+++ capacitor.config.ts
@@ -0,0 +1,16 @@
+
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.empresa.miapp',
+  appName: 'Mi Primera App',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
+
+
+
+
+
---
+++ src/router/index.ts
@@ -0,0 +1,72 @@
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { RouteRecordRaw } from 'vue-router';
+import BaseLayout from '@/layouts/BaseLayout.vue';
+import { useUserStore } from '@/stores/user';
+
+const Login = () => import('@/views/Login.vue');
+const Registro = () => import('@/views/Registro.vue');  
+const Camara = () => import('@/views/Camara.vue');
+const SeccionContenidos = () => import('@/views/SeccionContenido.vue');
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/seccion'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    meta: {
+      requiresAuth: false
+    },
+  },
+  {
+    path: '/registro',
+    name: 'Registro',
+    component: Registro,
+    meta: {
+      requiresAuth: false
+    },
+  },
+  {
+    path: '/camara',
+    name: 'Camara',
+    component: Camara,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/seccion',
+    name: 'Seccion',
+    component: BaseLayout,
+    meta: {
+      requiresAuth: true
+    },
+    children: [
+      {
+        path: ':name',
+        name: 'SeccionContenidos',
+        component: SeccionContenidos,
+      },
+    ]
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+
+router.beforeEach((to, from) => {
+  const userStore = useUserStore();
+  const isAuthenticated = !!userStore.token;
+  if(to.meta.requiresAuth && !isAuthenticated) {
+    return '/login';
+  }else if(isAuthenticated && !to.meta.requiresAuth) {
+    return '/seccion';
+  }
+});
+
+export default router
